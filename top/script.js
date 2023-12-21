39d84d0710e2f7dedaee841419a7184e7cc06393
samnart <tettey1234567@gmail.com>
2023-12-21 11:02:09 +0100

    method and function invocations

--- top/script.js
+++ top/script.js
@@ -241,3 +241,27 @@
 
 // console.log(numbers.sum())
 
+// const myObject = {
+//     // helloMethod is a method
+//     helloMethod: function() {
+//         return 'Hello World';
+//     }
+// };
+
+// const message = myObject.helloMethod();
+// console.log(message);
+
+const words = ['Hello', 'World'];
+words.join(', '); //method invocation
+
+const obj = {
+    myMethod() {
+        return new Date().toString();
+    }
+};
+obj.myMethod(); // method invocation
+
+const func = obj.myMethod;
+func(); // function invocation
+parseFloat('16.6'); // function invocation
+isNaN(0); // function invocation
