ea3c7c9f2845ff78ec3dac0700275f982b568902
samnart <tettey1234567@gmail.com>
2024-01-19 11:55:49 +0100

    node_modules

--- top/webpack-demo/src/index.js
+++ top/webpack-demo/src/index.js
@@ -1,13 +1,16 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
-    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    
     element.textContent = myName('Samuel');
+
+    element.classList.add('hello');
 
     return element
 }
