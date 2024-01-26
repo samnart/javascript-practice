b0ec373a8e091c271e5adf3ac9fdf4de7151e71e
samnart <tettey1234567@gmail.com>
2024-01-26 20:43:35 +0100

    promise.js index.html

--- top/chaps/promise.js
+++ top/chaps/promise.js
@@ -1,71 +1,129 @@
-// listen("click", function handler(evt){
-//     setTimeout( function request(){
-//         ajax( "http://some.url.1", function response(text){
-//             if (text == "hello") {
-//                 handler();
-//             }
-//             else if (text == "world") {
-//                 request();
-//             }
-//         });
-//     }, 500);
-// });
+// // listen("click", function handler(evt){
+// //     setTimeout( function request(){
+// //         ajax( "http://some.url.1", function response(text){
+// //             if (text == "hello") {
+// //                 handler();
+// //             }
+// //             else if (text == "world") {
+// //                 request();
+// //             }
+// //         });
+// //     }, 500);
+// // });
 
 
 
-// // A
-// ajax( "..", function(..){
-//     // C
-// });
-// // B
+// // // A
+// // ajax( "..", function(..){
+// //     // C
+// // });
+// // // B
 
 
-// analytics.trackPurchase( purchaseData, function() {
-//     chargeCreditCard();
-//     displayThankyouPage();
-// });
+// // analytics.trackPurchase( purchaseData, function() {
+// //     chargeCreditCard();
+// //     displayThankyouPage();
+// // });
 
-// var tracked = false;
+// // var tracked = false;
 
-// analytics.trackPurchase( purchaseData, function() {
-//     if (!tracked) {
-//         tracked = true;
-//         chargeCreditCard();
-//         displayThankyouPage();
+// // analytics.trackPurchase( purchaseData, function() {
+// //     if (!tracked) {
+// //         tracked = true;
+// //         chargeCreditCard();
+// //         displayThankyouPage();
+// //     }
+// // } );
+
+// function addNumbers(x, y) {
+//     if (typeof x != "number" || typeof y != "number") {
+//         throw Error( "Bad parameters" );
 //     }
-// } );
 
-function addNumbers(x, y) {
-    if (typeof x != "number" || typeof y != "number") {
-        throw Error( "Bad parameters" );
-    }
+//     return x + y;
+// }
 
-    return x + y;
-}
+// let sum = addNumbers( 21, '200' );
+// console.log(sum);
+// // addNumbers( 21, "21" );
 
-let sum = addNumbers( 21, '200' );
-console.log(sum);
-// addNumbers( 21, "21" );
+// function success(data) {
+//     console.log("Success! Data: ", data);
+// }
 
-function success(data) {
-    console.log("Success! Data: ", data);
-}
+// function failure(err) {
+//     console.error( err );
+// }
 
-function failure(err) {
-    console.error( err );
-}
+// ajax( "http://some.url.1", success, failure );
 
-ajax( "http://some.url.1", success, failure );
+// function response(err, data) {
+//     // error?
+//     if (err) {
+//         console.error( err );
+//     }
+//     // otherwise, assume success
+//     else {
+//         console.log( data );
+//     }
+// }
 
-function response(err, data) {
-    // error?
-    if (err) {
-        console.error( err );
-    }
-    // otherwise, assume success
-    else {
-        console.log( data );
-    }
-}
+// ajax( "http://some.url.1", response );
 
-ajax( "http://some.url.1", response );
+'use strict'
+
+// const { response } = require("express");
+
+// fetch('httpsa://api.chucknorris.io/jokes/randomasdf')
+//     .then((res) => {
+//         res.json().then((data) => {
+//             console.log(data);
+//         });
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+
+// function getWeather() {
+//     return new Promise(function(resolve, reject) {
+//         reject('Sunny');
+//     })
+// }
+
+// const promise = getWeather();
+
+// promise.then((data) => {
+//     console.log(data);
+// },
+// (data) => {
+//     console.log(`pokect slasher ${data}`);
+// })
+
+// console.log(promise.then());
+
+// function fetchData() {
+//     return new Promise( (resolve, reject) => {
+//         fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
+//             .then(response => response.json())
+//             .then(data => reject(data.properties.periods[13].shortForecast));
+//     } );
+// }
+
+// function displayData(weather) {
+//     console.log(weather);
+// }
+
+// function onError(err) {
+//     console.log(`ERRRORRR:`, err);
+// }
+
+// fetchData()
+//     .then(displayData)
+//     .catch(onError)
+
+const myDiv = document.querySelector('div#myDiv');
+
+myDiv.addEventListener("click", function() {
+    console.log("You clicked me!")
+})
