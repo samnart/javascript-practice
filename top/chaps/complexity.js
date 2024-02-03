18881cced41b67bd85d0eb488317e4eb9254630a
samnart <tettey1234567@gmail.com>
2024-02-03 19:29:08 +0100

    complexity.js

---
+++ top/chaps/complexity.js
@@ -0,0 +1,21 @@
+// function oddNumbersLessThanTen(maxNumber) {
+//     let currentNumber = 1;
+
+//     while(currentNumber < maxNumber) {
+//         if (currentNumber % 2 != 0) {
+//             console.log(currentNumber);
+//         }
+//         currentNumber++;
+//     }
+// }
+
+// oddNumbersLessThanTen()
+
+function findValue(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let item = arr[i];
+        if (item == 1) {
+            return item;
+        }
+    }
+}
