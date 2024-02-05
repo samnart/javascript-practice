027c5f17a73833b3c9118f96ef38cba8007f227f
samnart <tettey1234567@gmail.com>
2024-02-05 23:50:07 +0100

    got it done today, on to prepare for the exams

--- progressteps/script.js
+++ progressteps/script.js
@@ -1,28 +1,32 @@
-const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.querySelectorAll('.cirlce');
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
-let currentActive = 1;
+let currentActive = 1
 
 next.addEventListener('click', () => {
-    currentActive++;
+
+    currentActive++
 
     if(currentActive > circles.length) {
-        currentActive = circles.length;
+        currentActive = circles.length
     }
+
+    // console.log(currentActive)
 
     update();
 
 })
 
 prev.addEventListener('click', () => {
-    currentActive--;
+
+    currentActive--
 
     if(currentActive < 1) {
-        currentActive = 1;
+        currentActive = 1
     }
-    update();
+    update()
 })
 
 function update() {
@@ -33,4 +37,19 @@ function update() {
             circle.classList.remove('active')
         }
     })
+
+    const actives = document.querySelectorAll('.active')
+
+    // console.log(actives.length, circles.length)
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if (currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
 }
