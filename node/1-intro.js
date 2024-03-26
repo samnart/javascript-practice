8720b6304155111a68fc3877ea11782fc48adfc2
samnart <tettey1234567@gmail.com>
2024-03-26 15:22:01 +0100

    node basics

---
+++ node/1-intro.js
@@ -0,0 +1,10 @@
+const number = 12;
+
+if (number < 12) {
+    console.log('small number');
+} 
+else {
+    console.log('big number');
+}
+
+console.log(`starting with nodejs`);
---
+++ node/3-modules.js
@@ -0,0 +1,24 @@
+// Modules
+// CommonJS, every file is a module (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+// const utils = require('./5-utils')
+
+
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alt')
+require('./7-mind-grenade')
+
+
+
+
+
+
+// console.log(names);
+// sayHi('Samuel')
+// sayHi(names.john)
+// sayHi(names.peter)
+
+// console.log();
+
