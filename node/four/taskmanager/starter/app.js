b881cff44fd579dabcb3d859b51a10f93de01cf1
samnart <tettey1234567@gmail.com>
2024-04-03 13:10:47 +0200

    task manager node app

--- node/four/taskmanager/starter/app.js
+++ node/four/taskmanager/starter/app.js
@@ -1,12 +1,21 @@
 const express = require('express')
-
 const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
 const port = 3000
+
+// middleware
+
+app.use(express.json())
 
 // routes 
 app.get('/hello', (req, res) => {
     res.send('Task Manager App');
 })
+
+app.use('/api/v1/tasks', tasks)
 
 // app.get('/api/v1/tasks')                 - get all the tasks
 // app.post('/api/v1/tasks')                - create a new task
@@ -14,6 +23,13 @@ app.get('/hello', (req, res) => {
 // app.patch('/api/v1/tasks/:id')           - update task
 // app.delete('/api/v1/tasks/:id')          - delete task   
 
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-
-app.listen(port, console.log(`Server is listening on port ${port}...`))
+start()
